91e5bd8cc480549bf387668c7108eef7c5456b37
pragati200 <90255949+pragati200@users.noreply.github.com>
2021-11-02 14:28:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function clickMe(){
     alert("Welcome to the website, here we will learn how to link javascript to HTML files");
 }
+
+function clickMeExt(){
+    alert("This is an example of linking \"EXTERNAL FILE\" ");
+}
